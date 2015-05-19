3834b31c3da89b19c3afdd85799c6f48ec10a458
Lucian Hontau <lucian303@gmail.com>
2015-05-18 23:18:24 -0400

    Removing extra files. Fixing test.

--- test/corenlpd/test/handler.clj
+++ test/corenlpd/test/handler.clj
@@ -4,8 +4,8 @@
         corenlpd.handler))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/"))]
+  (testing "main nlp route"
+    (let [response (app (request :get "/parse?text=This+is+a+test."))]
       (is (= 200 (:status response)))))
 
   (testing "not-found route"
