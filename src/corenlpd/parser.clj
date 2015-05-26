9deb899f8f6944eba5d79bad0a5552b3c26ef87a
Lucian Hontau <lucian303@gmail.com>
2015-05-26 12:49:56 -0400

    Getting raw parser data for text with POS.

--- src/corenlpd/parser.clj
+++ src/corenlpd/parser.clj
@@ -1,19 +1,24 @@
 (ns corenlpd.parser
-  (:import
-    (java.io StringWriter)
-    (java.util Properties)
-    (edu.stanford.nlp.pipeline Annotation StanfordCoreNLP))
-  (:require [compojure.api.sweet :refer [GET*]]))
+  (:require [clojure.string :refer [split]])
+  (:import (java.io StringWriter PrintWriter)
+    (java.util Properties ArrayList)
+    (edu.stanford.nlp.ling TaggedWord)
+    (edu.stanford.nlp.pipeline Annotation StanfordCoreNLP)
+    (edu.stanford.nlp.parser.lexparser LexicalizedParser)
+    (edu.stanford.nlp.trees TreePrint)))
 
 (def annotators-by-type {:parse-full "tokenize, ssplit, pos, lemma, parse"})
+
+(def models {:englishPCFG "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"})
 
 (defn get-props [type]
   (let [props (Properties.)]
     (.setProperty props "annotators" (type annotators-by-type))
     props))
 
-(defn parse [text type]
+(defn parse
   "Parse some text using Stanford CoreNLP"
+  [text type]
   (let [annot (Annotation. text)
         props (get-props type)
         pipeline (StanfordCoreNLP. props)
@@ -21,3 +26,22 @@
     (.annotate pipeline annot)
     (.xmlPrint pipeline annot output)
     (.toString output)))
+
+(defn parse-with-pos
+  "Reparse text already tagged with slashes and parts of speech to get
+  updated relationships. Words and relationships should be seperated by
+  a slash '/'. There should be a space between all words and punctuation."
+  [text model]
+  (let [parser (. LexicalizedParser loadModel (model models) [])
+        tokens (split text #"(\s|/)")
+        words (take-nth 2 tokens)
+        tags (take-nth 2 (rest tokens))
+        tagged-words (zipmap words tags)
+        sentence (ArrayList.)]
+    (dorun (map (fn [[key val]] (.add sentence (TaggedWord. key val))) (seq tagged-words)))
+    (let [tree (.parse parser sentence)
+          tp (TreePrint. "wordsAndTags, typedDependencies")
+          output (StringWriter.)
+          pw (PrintWriter. output false)]
+    (.printTree tp tree pw)
+    (.toString output))))
--- src/corenlpd/routes/home.clj
+++ src/corenlpd/routes/home.clj
@@ -1,13 +1,11 @@
 (ns corenlpd.routes.home
   (:require [corenlpd.parser :as parser]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes]]
             [compojure.api.sweet :refer [GET*]]
-            [ring.util.http-response :refer [ok]]
             [ring.util.response :refer [response content-type]]))
 
-(defn route-parse [request type]
-   (let [text (get (:params request) :text)
-         xml (parser/parse text type)]
+(defn route-parse [text type]
+   (let [xml (parser/parse text type)]
          (-> xml
             response
             (content-type "text/xml"))))
@@ -17,4 +15,10 @@
     :return       String
     :query-params [text :- String]
     :summary      "Process some text through Core NLP."
-    (fn [req] (route-parse req :parse-full))))
+    (fn [req] (route-parse text :parse-full)))
+
+  (GET* "/parse-with-pos" []
+    :return       String
+    :query-params [text :- String]
+    :summary      "Process some text already tagged with parts of speech through Core NLP."
+    (fn [req] (parser/parse-with-pos text :englishPCFG))))
